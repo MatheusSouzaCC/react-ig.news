7737b48928d2fff99e4651fa267cde898218887c
Matheus de Souza <matheusdesouza078@gmail.com>
2022-06-18 17:12:24 -0300

    Autenticação com GitHub

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,28 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    //Renomeando o novo objeto chamado "data" para ter o nome "session" que é usado durante a aula:
+    const { data: session } = useSession();
 
-    return isUserLoggedIn ? (
+    console.log(session);
+
+    return session ? (
         <button
             type="button"
             className={styles.signInButton}
         >
             <FaGithub color="#04d361" />
-            Matheus de Souza
-            <FiX color="#737380" className={styles.closeIcon} />
+            {session.user.name}
+            <FiX color="#737380" className={styles.closeIcon} onClick={() => signOut()} />
         </button>
     ) : (
         <button
             type="button"
             className={styles.signInButton}
+            onClick={() => signIn('github')}
         >
             <FaGithub color="#eba417" />
             Sign in with Github
